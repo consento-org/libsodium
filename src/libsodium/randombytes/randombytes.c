64615fdf4d7eee30b1cd938ac9cae9faff461387
Martin Heidegger <martin.heidegger@gmail.com>
2019-10-02 14:59:07 +0900

    Using @consento/sync-randombytes to have fallbacks for all systems.

--- src/libsodium/randombytes/randombytes.c
+++ src/libsodium/randombytes/randombytes.c
@@ -86,29 +86,11 @@ randombytes_stir(void)
 #else
     EM_ASM({
         if (Module.getRandomValue === undefined) {
-            try {
-                var window_ = 'object' === typeof window ? window : self;
-                var crypto_ = typeof window_.crypto !== 'undefined' ? window_.crypto : window_.msCrypto;
-                var randomValuesStandard = function() {
-                    var buf = new Uint32Array(1);
-                    crypto_.getRandomValues(buf);
-                    return buf[0] >>> 0;
-                };
-                randomValuesStandard();
-                Module.getRandomValue = randomValuesStandard;
-            } catch (e) {
-                try {
-                    var crypto = require('crypto');
-                    var randomValueNodeJS = function() {
-                        var buf = crypto['randomBytes'](4);
-                        return (buf[0] << 24 | buf[1] << 16 | buf[2] << 8 | buf[3]) >>> 0;
-                    };
-                    randomValueNodeJS();
-                    Module.getRandomValue = randomValueNodeJS;
-                } catch (e) {
-                    throw 'No secure random number generator found';
-                }
-            }
+            Module.getRandomValue = (function () {
+                var rnd = require('@consento/sync-randombytes');
+                var buf = new Uint32Array(1);
+                return function () { return (rnd(buf)[0]) >>> 0 };
+            })();
         }
     });
 #endif
